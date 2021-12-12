4fbcdcc8ff5a3d0f0f8da0d0537ea89a19b25a06
Aditya <aditya.jadhav@comp.sce.edu.in>
2021-12-12 22:03:43 +0530

    Initial commit

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require('mongoose');
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 const port = 80;
 
-async function main() {
-    await mongoose.connect('mongodb://localhost:27017/order');
-}
+mongoose.connect('mongodb://localhost:27017/order')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+ 
 const orderSchema = new mongoose.Schema({
     Name: String,
     Mobile: String,
@@ -21,36 +23,32 @@ const orderSchema = new mongoose.Schema({
 });
 const Order = mongoose.model('Order', orderSchema);
 
-// EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
-app.use('/images', express.static(__dirname + '../static/images'))
-app.use('/js', express.static(__dirname + 'static/js'))
 
-// PUG SPECIFIC STUFF
-app.set('view engine', 'pug') // Set the template engine as pug
-app.set('views', path.join(__dirname, 'views')) // Set the views directory
-
-// ENDPOINTS
-app.get('/', (req, res) => {
-    const params = {}
-    res.status(200).render('index.pug', params);
-})
+app.get('/',(_req, res) => {
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+  });
+  
 
 app.post('/order', (req, res)=>{
+    console.log(req.body);
     var myData = new Order(req.body);
-    myData.save().then(()=>{
-    res.send("This item has been saved to the database")
-    }).catch(()=>{
-    res.status(400).send("item was not saved to the databse")
-})
-    // res.redirect('/');
-})
-
+    myData.save();
+    res.redirect('/');
+});
 
 // START THE SERVER
 app.listen(port, () => {
     console.log(`The application started successfully on port ${port}`);
 });
+
+
+
+
+
+
+
+
 
 
 
